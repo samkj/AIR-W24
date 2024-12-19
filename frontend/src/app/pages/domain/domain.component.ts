9d2e56ef88c9f2934857234098d981198e677f26
Christian Bachl <chrisibachl13@gmail.com>
2024-12-20 00:48:51 +0100

    forgot to add confirmdialog

--- frontend/src/app/pages/domain/domain.component.ts
+++ frontend/src/app/pages/domain/domain.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConfirmDialog } from 'primeng/confirmdialog';
-import { Toast } from 'primeng/toast';
+import { Toast, ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-domain',
   standalone: true,
-  imports: [RouterOutlet, Toast],
+  imports: [RouterOutlet, Toast, ConfirmDialog, ToastModule],
   templateUrl: './domain.component.html',
   styleUrl: './domain.component.scss',
   host: {
