92d2d997eb0eb4f0bad168389300b94cb2ae4a1f
Christian Bachl <chrisibachl13@gmail.com>
2024-12-31 14:34:09 +0100

    added pull in ollama start script

--- model_files/run_ollama.sh
+++ model_files/run_ollama.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 echo "Starting Ollama server..."
-ollama serve &  # Start Ollama in the background
+ollama serve &
+
+echo "Waiting for Ollama server to be active..."
+while [ "$(ollama list | grep 'NAME')" == "" ]; do
+  sleep 1
+done
 
 echo "Ollama is ready, starting the model..."
 
+ollama pull ksamirk/customer-support-quantized
+
 ollama run ksamirk/customer-support-quantized
 
-echo "Waiting for Ollama server to be active..."
 while true; do
   sleep 1
 done
